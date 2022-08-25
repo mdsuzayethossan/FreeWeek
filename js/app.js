62963a51a1826f7e3f009c859ed26c32265ea5d8
Md Suzayet Hossan <md.suzayet.377@gmail.com>
2022-08-25 10:15:49 +0600

    third

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () =>
-container.classList.add('right-panel-active'));
-
-signInButton.addEventListener('click', () =>
-container.classList.remove('right-panel-active'));
+document.getElementById('header-dropdown-btn').addEventListener('click', function(){
+    const headerDropdownMenus = document.getElementById('header-dropdown-menus');
+    headerDropdownMenus.classList.toggle("show");
+});
