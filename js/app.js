2cc9d81ef6894eb59abb871d1996a538bd459f85
Md Suzayet Hossan <md.suzayet.377@gmail.com>
2022-08-26 12:07:01 +0600

    search

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
-document.getElementById('header-dropdown-btn').addEventListener('click', function(){
-    const headerDropdownMenus = document.getElementById('header-dropdown-menus');
-    headerDropdownMenus.classList.toggle("show");
-});
+
+(() => {
+    'use strict'
+  
+    document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
+      document.querySelector('.offcanvas-collapse').classList.toggle('open')
+    });
+
+    //
+    document.getElementById('header-dropdown-btn').addEventListener('click', function(){
+        const headerDropdownMenus = document.getElementById('header-dropdown-menus');
+        headerDropdownMenus.classList.toggle("show");
+    });
+  })()
+  
